401a62bb747a9089f02dfca6a39cccff2373decf
hav0x45 <pekkakukonlehto@outlook.com>
2025-03-28 09:19:19 +0200

    A wild template file found!

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-using namespace std;
 
 // main() function: where the execution of the program begins
 integer main() {
   
-    // This statement prints "Hello World"
-    cout << "Hello world!";
+    // This statement prints "Hello world!"...
+    std::cout << "Hello world!";
 
     return 0;
 }
